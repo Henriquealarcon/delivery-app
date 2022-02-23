696877b17c01bcc044ebf57d77e33d89dcf3e5df
Henrique Alarcon <henrique.alarcon@hotmail.com>
2022-02-23 12:43:25 -0300

    feat: implementa o service e middleware de autenticaçao

---
+++ back-end/src/services/authentication/authentication.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+
+const { JWT_SECRET } = process.env;
+
+const JWT_CONFIG = {
+  expiresIn: '15m',
+  algorithm: 'HS256',
+};
+
+const genToken = (data) => {
+  const token = jwt.sign({ data }, JWT_SECRET, JWT_CONFIG);
+  return token;
+};
+
+const verifyToken = (token) => {
+    const decoded = jwt.verify(token, JWT_SECRET);
+    const user = decoded.data;
+
+    return user;
+};
+
+module.exports = {
+  genToken,
+  verifyToken,
+};
