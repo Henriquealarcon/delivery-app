affaefab55028396f4f0f5f318ba63b789c7d99e
igor <igorsife80@gmail.com>
2022-02-23 15:24:04 -0300

    feat: adiciona funcionalidade enable/desable do botão de login

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -1,24 +1,46 @@
-import React, {/* useState */} from 'react';
+import React, { useState } from 'react';
+import { loginValidation } from '../utils/inputValidations';
 
 export default function Login() {
-  // const [displayLog, setDisplayLog] = useState('hidden');
+  const [login, setLogin] = useState({
+    email: '',
+    password: '',
+  });
+
+  const validatePassword = ({ target: { name, value } }) => {
+    setLogin({ ...login,
+      [name]: value,
+    });
+  };
+
+  function handleLoginValidation() {
+    const validationError = loginValidation(login).error;
+    if (validationError) return true;
+    return false;
+  }
+
   return (
     <>
       <p
         data-testid="common_login__element-invalid-email"
-        display="hidden"
+        display="hiden"
       >
         invalid credential
       </p>
       <input
+        name="email"
+        onBlur={ validatePassword }
         type="text"
         data-testid="common_login__input-email"
       />
       <input
-        type="text"
+        name="password"
+        onBlur={ validatePassword }
+        type="password"
         data-testid="common_login__input-password"
       />
       <button
+        disabled={ handleLoginValidation() }
         type="submit"
         data-testid="common_login__button-login"
       >
