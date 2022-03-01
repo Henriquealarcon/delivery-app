5ef08af042bc321dfe22604a07650ee8d2af4168
José Demeneghi <josedemeneghi_cba@hotmail.com>
2022-03-01 16:27:12 +0000

    feat: adiciona as rotas iniciais para admin e seller, paginas ainda em construcao

---
+++ front-end/src/pages/adm/Management.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Navbar from '../../components/Navbar';
+
+export default function Management() {
+  return (
+    <div>
+      <Navbar />
+      aqui ficará admin Management
+    </div>
+  );
+}
---
+++ front-end/src/pages/sellers/SellerOrders.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Navbar from '../../components/Navbar';
+
+export default function SellerOrders() {
+  return (
+    <div>
+      <Navbar />
+      aqui ficará orders
+    </div>
+  );
+}
