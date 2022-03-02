e9deac657155aeadace1c006ac15a36231c3cae0
José Demeneghi <josedemeneghi_cba@hotmail.com>
2022-03-02 10:48:42 +0000

    feat: cria um estado para armazenar os dados do user e insere a funcao que o altera quando os dados sao inseridos

--- front-end/src/pages/adm/Management.jsx
+++ front-end/src/pages/adm/Management.jsx
@@ -1,35 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from '../../components/Navbar';
 
 export default function Management() {
+  const [register, setRegister] = useState({
+    name: '',
+    email: '',
+    password: '',
+    role: 'customer',
+  });
+
+  const validatePassword = ({ target: { name, value } }) => {
+    setRegister({ ...register,
+      [name]: value,
+    });
+  };
+
   return (
     <div>
       <Navbar />
       <input
         name="name"
-        // onChange={}
+        onChange={ validatePassword }
         type="text"
         data-testid="admin_manage__input-name"
         placeholder="Nome e sobrenome"
       />
       <input
         name="email"
-        // onChange={}
+        onChange={ validatePassword }
         type="text"
         data-testid="admin_manage__input-email"
         placeholder="E-mail"
       />
       <input
         name="password"
-        // onChange={}
+        onChange={ validatePassword }
         type="password"
         data-testid="admin_manage__input-password"
         placeholder="Insira sua senha"
       />
-      <select name="role" data-testid="admin_manage__select-role">
+      <select
+        name="role"
+        onChange={ validatePassword }
+        data-testid="admin_manage__select-role"
+      >
         <option value="customer">Cliente</option>
         <option value="seller">Vendedor</option>
-        <option value="admin">Admin</option>
+        <option value="administrator">Admin</option>
       </select>
       <button
         // disabled={}
