3349931280923c59abc17962f69c8ca09b483ee4
José Demeneghi <josedemeneghi_cba@hotmail.com>
2022-03-02 17:23:08 +0000

    feat: implementa o envio do headers pelo axios

--- front-end/src/services/AdminRegisterServices.js
+++ front-end/src/services/AdminRegisterServices.js
@@ -2,11 +2,16 @@ import axios from 'axios';
 
 const notExist = 404;
 
-async function apiRegister(register) {
+async function apiRegister(register, token) {
   try {
     const url = 'http://localhost:3001/adminRegister';
+    const config = {
+      headers: {
+        authorization: token,
+      },
+    };
 
-    const fetchAPI = await axios.post(url, register);
+    const fetchAPI = await axios.post(url, register, config);
     const response = await fetchAPI.data;
     return response;
   } catch (error) {
