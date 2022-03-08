ebd4af12690d0a8422695dee9343bda8206e3638
jmlinger <jmlinger10@gmail.com>
2022-03-07 23:01:04 -0300

    fix: add o middware auth a rota de sales

--- back-end/src/controllers/sales/router.js
+++ back-end/src/controllers/sales/router.js
@@ -1,11 +1,12 @@
 const express = require('express');
 
 const rescue = require('express-rescue');
+const authentication = require('../../middlewares/authentication');
 
 const router = express.Router({ mergeParams: true });
 
 const create = require('./create');
 
-router.post('/', rescue(create));
+router.post('/', rescue(authentication), rescue(create));
 
 module.exports = router;
