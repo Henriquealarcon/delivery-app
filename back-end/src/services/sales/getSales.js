6be12a316e07de9b07c8d6ab589b2da74e9e44db
jmlinger <jmlinger10@gmail.com>
2022-03-09 17:16:44 -0300

    fix: altera retorno da data de criacao de vendas

--- back-end/src/services/sales/getSales.js
+++ back-end/src/services/sales/getSales.js
@@ -1,11 +1,19 @@
 const { StatusCodes } = require('http-status-codes');
+const { sequelize } = require('../../database/models');
 const Models = require('../../database/models');
 
+const attributes = {
+  include: [
+    [sequelize.fn('DATE_FORMAT',
+      sequelize.col('sale_date'), '%d/%m/%Y'), 'saleDate'],
+  ],
+};
+
 module.exports = async (id) => {
-    const getSales = await Models.sales.findAll({ where: { userId: id } });
+    const getSales = await Models.sales.findAll({ where: { userId: id }, attributes });
 
     if (getSales.length === 0) {
-    const getSalesFromSeller = await Models.sales.findAll({ where: { sellerId: id } });
+    const getSalesFromSeller = await Models.sales.findAll({ where: { sellerId: id }, attributes });
     
     return { 
       status: StatusCodes.OK, message: getSalesFromSeller,
