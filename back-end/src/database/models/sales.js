65274f53f7792fe36867c517b0b88b2de6a13f55
Henrique Alarcon <henrique.alarcon@hotmail.com>
2022-03-09 20:11:22 -0300

    fix: altera o model de sales para arrumar as associations

--- back-end/src/database/models/sales.js
+++ back-end/src/database/models/sales.js
@@ -19,8 +19,9 @@ module.exports = (sequelize, DataTypes) => {
 
   sales.associate = (models) => {
     sales.belongsTo(models.users,
-      { foreignKey: 'userId', as: 'user' },
-      { foreignKey: 'sellerId', as: 'user' });
+      { foreignKey: 'userId', as: 'user' });
+    sales.belongsTo(models.users,
+      { foreignKey: 'sellerId', as: 'seller' });
   };
 
   return sales;
