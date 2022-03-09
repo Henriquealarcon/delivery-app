ee597c44b57d0eba8794e6bd8f4bc6e64d829d58
Henrique Alarcon <henrique.alarcon@hotmail.com>
2022-03-09 20:08:19 -0300

    fix: altera o fetch na api para buscar as informacoes especificas de cada venda

--- front-end/src/services/ApiSalesService.js
+++ front-end/src/services/ApiSalesService.js
@@ -2,13 +2,8 @@ import axios from 'axios';
 
 const getSaleById = async (id) => {
   try {
-    const url = 'http://localhost:3001/customer/order';
-    const { token } = JSON.parse(localStorage.getItem('user'));
-    const fetchAPI = await axios.get(url, { params: { id } }, { headers: {
-      // eslint-disable-next-line
-      Authorization: token,
-    },
-    });
+    const url = `http://localhost:3001/customer/order/sales/${id}`;
+    const fetchAPI = await axios.get(url);
     const response = await fetchAPI.data;
 
     return response;
