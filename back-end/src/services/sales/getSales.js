30fc1cda9186671cf4cc559138503ddddcf85107
igor <igorsife80@gmail.com>
2022-03-09 15:44:49 -0300

    feat: implementa a tela de pedidos do vendedor

--- back-end/src/services/sales/getSales.js
+++ back-end/src/services/sales/getSales.js
@@ -1,8 +1,17 @@
 const { StatusCodes } = require('http-status-codes');
 const Models = require('../../database/models');
 
-module.exports = async (userId) => {
-    const getSales = await Models.sales.findAll({ where: { userId } });
+module.exports = async (id) => {
+    const getSales = await Models.sales.findAll({ where: { userId: id } });
+
+    if (getSales.length === 0) {
+    const getSales = await Models.sales.findAll({ where: { sellerId: id } });
+    
+    return { 
+      status: StatusCodes.OK, message: getSales,
+     };
+    }
+
     return { 
       status: StatusCodes.OK, message: getSales,
      };
--- front-end/src/pages/sellers/SellerOrders.jsx
+++ front-end/src/pages/sellers/SellerOrders.jsx
@@ -1,11 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import Navbar from '../../components/Navbar';
+import getOrders from '../../services/ApiOrdersService';
 
 export default function SellerOrders() {
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    const get = async () => {
+      const response = await getOrders();
+      setOrders(response.message);
+    };
+    get();
+  }, []);
+
   return (
     <div>
       <Navbar />
-      aqui ficará orders
+      {
+        orders.map((order, index) => (
+          <Link
+            to={ `/seller/orders/${order.id}` }
+            key={ index }
+          >
+            <div
+              key={ index }
+            >
+              <p data-testid={ `seller_orders__element-order-id-${order.id}` }>
+                { order.id }
+              </p>
+              <p data-testid={ `seller_orders__element-delivery-status-${order.id}` }>
+                { order.status }
+              </p>
+              <p data-testid={ `seller_orders__element-order-date-${order.id}` }>
+                { order.saleDate }
+              </p>
+              <p data-testid={ `seller_orders__element-card-price-${order.id}` }>
+                { order.totalPrice.replace('.', ',') }
+              </p>
+            </div>
+            <p data-testid={ `seller_orders__element-card-address-${order.id}` }>
+              order.adressa
+              {console.log(order)}
+            </p>
+          </Link>
+        ))
+      }
     </div>
   );
 }
