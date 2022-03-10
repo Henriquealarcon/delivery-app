893db3f3771300a41c4875e907b1b4db56d86b69
Henrique Alarcon <henrique.alarcon@hotmail.com>
2022-03-09 21:15:15 -0300

    fix: formato da data no back-end

--- back-end/src/services/sales/getSalesById.js
+++ back-end/src/services/sales/getSalesById.js
@@ -1,8 +1,17 @@
 const { StatusCodes } = require('http-status-codes');
+const sequelize = require('sequelize');
 const Models = require('../../database/models');
+
+const attributes = {
+    include: [
+      [sequelize.fn('DATE_FORMAT',
+       sequelize.col('sale_date'), '%d/%m/%Y'), 'saleDate'],
+    ],
+  };
 
 module.exports = async (id) => {
   const getSalesById = await Models.sales.findByPk(id, { 
+    attributes,
     include: [
   { model: Models.users, as: 'seller', attributes: { exclude: ['email', 'password'] } },
   { model: Models.users, as: 'user', attributes: { exclude: ['email', 'password'] } },
